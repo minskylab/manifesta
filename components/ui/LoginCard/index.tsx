0ab28f4ae2ea169f375783954287725bdd83bee1
Max Landeo <maxlr2005@gmail.com>
2020-12-05 05:17:18 -0500

    feat: register frontend almost ready

--- components/ui/LoginCard/index.tsx
+++ components/ui/LoginCard/index.tsx
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-// import {
-//     Box,
-//     ThemeProvider,
-//     theme,
-//     ColorModeProvider,
-//     CSSReset,
-//     Flex,
-//     IconButton,
-//     useColorMode,
-//     Heading,
-//     Link,
-//     Text,
-//     FormControl,
-//     FormLabel,
-//     Input,
-//     Stack,
-//     Checkbox,
-//     Button,
-// } from "@chakra-ui/core";
-=======
 import {
     Box,
     ThemeProvider,
@@ -33,100 +12,12 @@ import {
     Checkbox,
     Button,
 } from "@chakra-ui/react";
->>>>>>> feature/login
 
  import React from "react";
-// import "@emotion/react";
+ import "@emotion/react";
 
-// const VARIANT_COLOR = "teal";
+const VARIANT_COLOR = "teal";
 
-<<<<<<< HEAD
-// const LoginCard = () => {
-//     return (
-//         <ThemeProvider theme={theme}>
-//             <ColorModeProvider>
-//                 <CSSReset>
-//                     <LoginArea />
-//                 </CSSReset>
-//             </ColorModeProvider>
-//         </ThemeProvider>
-//     );
-// };
-// const LoginArea = () => {
-//     return (
-//         <Flex minHeight="100wh" width="full" align="center" justifyContent="center">
-//             <Box
-//                 borderWidth={1}
-//                 px={4}
-//                 width="full"
-//                 maxWidth="500px"
-//                 borderRadius={4}
-//                 textAlign="center"
-//                 boxShadow="lg"
-//             >
-//                 <ThemeSelector />
-//                 <Box p={4}>
-//                     <LoginHeader />
-//                     <LoginForm />
-//                 </Box>
-//             </Box>
-//         </Flex>
-//     );
-// };
-
-// const ThemeSelector = () => {
-//     const { colorMode, toggleColorMode } = useColorMode();
-//     return (
-//         <Box textAlign="right" py={4}>
-//             <IconButton
-//                 aria-label="Search database"
-//                 icon={colorMode === "light" ? "moon" : "sun"}
-//                 onClick={toggleColorMode}
-//                 variant="ghost"
-//             />
-//         </Box>
-//     );
-// };
-// const LoginHeader = () => {
-//     return (
-//         <Box>
-//             <Heading>Sign In Your Account</Heading>
-//             <Text>
-//                 Or<Link color={`${VARIANT_COLOR}.500`}>Start your 14 days trial</Link>
-//             </Text>
-//         </Box>
-//     );
-// };
-
-// const LoginForm = () => {
-//     return (
-//         <Box my={8} textAlign="left">
-//             <form>
-//                 <FormControl>
-//                     <FormLabel>Email address</FormLabel>
-//                     <Input type="email" placeholder="Enter your email address" />
-//                 </FormControl>
-//                 <FormControl mt={4}>
-//                     <FormLabel>Password</FormLabel>
-//                     <Input type="password" placeholder="Enter your password" />
-//                 </FormControl>
-//                 <Stack isInline justifyContent="space-between" mt={4}>
-//                     <Box>
-//                         <Checkbox>Remenber Me</Checkbox>
-//                     </Box>
-//                     <Box>
-//                         <Link color={`${VARIANT_COLOR}.500`}>Forgot your password=</Link>
-//                     </Box>
-//                 </Stack>
-//                 <Button variantColor={VARIANT_COLOR} width="full" mt={4}>
-//                     Sing In
-//                 </Button>
-//             </form>
-//         </Box>
-//     );
-// };
-// export default LoginCard;
-=======
 const LoginCard = () => {
     return (
         <ThemeProvider theme={theme}>
@@ -191,4 +82,3 @@ const LoginForm = () => {
     );
 };
 export default LoginCard;
->>>>>>> feature/login
