6d0ed4f3fa510fc1b26888744c0ad502a9169b25
Bregy Malpartida <bregy.malpartida@utec.edu.pe>
2020-12-05 01:28:50 -0500

    feat (login): New login page

--- pages/login.tsx
+++ pages/login.tsx
@@ -2,6 +2,8 @@ import { Box } from "@chakra-ui/react";
 import { NextPage } from "next";
 import React from "react";
 
+// TODO: Put your login card here
+
 const LoginPage: NextPage = () => {
     return <Box>Login Page</Box>;
 };
